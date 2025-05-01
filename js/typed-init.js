ca657d4fedd84a157e9aadd51586bd12d5704d28
Yazan550 <thyyazen3@gmail.com>
2025-05-01 23:37:34 +0300

    Update and rename typed-widget.js to typed-init.js

--- js/typed-init.js
+++ js/typed-init.js
@@ -1,9 +1,10 @@
+// typed-init.js
 document.addEventListener("DOMContentLoaded", function() {
   if (typeof jQuery !== 'undefined' && jQuery().typed) {
     $(".header-logo-desc p").typed({
       strings: [
         "شعارك الإحترافي الآن",
-        "هوية بصرية مبتكرة", 
+        "هوية بصرية مبتكرة",
         "رؤية تترجم نجاحك",
         "استراتيجيات ترفع المبيعات",
         "تصاميم تعزز الثقة"
