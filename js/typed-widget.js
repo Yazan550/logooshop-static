967d894bde83c848331051ae1e4f6d36b2a67d40
Yazan550 <thyyazen3@gmail.com>
2025-05-01 21:57:36 +0300

    Update typed-widget.js

--- js/typed-widget.js
+++ js/typed-widget.js
@@ -1,15 +1,16 @@
-$(function () {
-  "use strict";
-  $(".header-logo-desc p").typed({
-    strings: [
-      "شعارك الإحترافي الآن",
-      "هوية بصرية مبتكرة",
-      "رؤية تترجم نجاحك",
-      "استراتيجيات ترفع المبيعات",
-      "تصاميم تعزز الثقة"
-    ],
-    loop: true,
-    startDelay: 1000,
-    backDelay: 2000
-  });
+document.addEventListener("DOMContentLoaded", function() {
+  if (typeof jQuery !== 'undefined' && jQuery().typed) {
+    $(".header-logo-desc p").typed({
+      strings: [
+        "شعارك الإحترافي الآن",
+        "هوية بصرية مبتكرة", 
+        "رؤية تترجم نجاحك",
+        "استراتيجيات ترفع المبيعات",
+        "تصاميم تعزز الثقة"
+      ],
+      loop: true,
+      startDelay: 1000,
+      backDelay: 2000
+    });
+  }
 });
